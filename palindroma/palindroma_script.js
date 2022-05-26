8ef95e58e5707fbbf0e1b98e13daeec6cb746fbb
silviawolves <silviacanepa.w@gmail.com>
2022-05-26 15:29:50 +0200

    in progress

--- palindroma/palindroma_script.js
+++ palindroma/palindroma_script.js
@@ -1,6 +1,30 @@
 'use strict'
 
+//faccio inserire all'utente una parola
+const parolaUtente = prompt('Inserisci una parola')
 
+//creo una variabile vuota che si confermi se le condizioni si realizzano
+let palindromia = false
+
+//devo creare una funziona che scomponga la parola inserita in lettere
+//che scorra dentro gli array 
+
+function isPalindroma(parolaUtente) {
+    //scompongo quella parola lettera per lettera e creo una costante
+    const parolaScomposta = parolaUtente.split("")
+    console.log(parolaScomposta)
+
+    for (let i = 0; i < parolaScomposta.length; i++) {
+        console.log(parolaScomposta[i])
+    }
+
+    for (let k = parolaScomposta.length - 1; k <= 0; k--) {
+        console.log(parolaScomposta[k])
+    }
+
+}
+
+console.log(isPalindroma(parolaUtente))
 
 /*
 Chiedere all’utente di inserire una parola
