b97983b309aeaf6d622fe827ab5899f83d1c4880
silviawolves <silviacanepa.w@gmail.com>
2022-05-26 13:46:49 +0200

    codici base + consegne

---
+++ palindroma/palindroma_script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+
+
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
---
+++ pari&dispari/pari-dispari_script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+
+
+
+/*
+L’utente sceglie pari o dispari tramite un prompt
+L’utente inserisce anche un numero da 1 a 5.
+Generiamo poi un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri (quello inserito dall’utente e quello random del computer)
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. Se l’utente aveva scelto pari e la somma è pari, ha vinto l’utente, altrimenti il computer
+*/
